c7785e67ccea4e911fef97ce6b664d3facb35280
nilswen <nilswentingpro@gmail.com>
2023-09-07 00:47:04 +0200

    .

--- jour1/Job07/FormesTest.java
+++ jour1/Job07/FormesTest.java
@@ -3,7 +3,7 @@ package jour1.Job07;
 public class FormesTest {
     public static void main(String[] args) {
         Carre carre = new Carre(4);
-        Cercle cercle = new Cercle(3);
+        Cercle cercle = new Cercle(5);
         carre.CalculAireCarre();
         cercle.CalculAireCercle();
     }
