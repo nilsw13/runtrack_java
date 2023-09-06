39d170be27f6ddf26cdafaf8286b19013aff1789
nilswen <nilswentingpro@gmail.com>
2023-09-06 14:39:12 +0200

    commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez saisir votre nom: ");
+        String userName = scanner.nextLine();
+        System.out.println("hello, "+ userName + "!");
+        scanner.close();
+    }
+}
