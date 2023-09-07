60b2138406655fa4218d31ea03c9c44c499ec504
nilswen <nilswentingpro@gmail.com>
2023-09-07 15:37:28 +0200

    .

---
+++ jour2/BanqueExercice.java
@@ -0,0 +1,38 @@
+package jour2;
+import jour1.Job04.CompteBancaire;
+
+public class BanqueExercice extends CompteBancaire {
+    double soldeCourant;
+    double soldeEpargne;
+
+    double tauxInteret;
+
+
+
+    public BanqueExercice(double soldeCourant, double soldeEpargne, double tauxInteret){
+        this.soldeCourant = soldeCourant;
+        this.soldeEpargne = soldeEpargne;
+        this.tauxInteret = tauxInteret;
+        System.out.println("Solde du compte courant : " + soldeCourant + "€");
+        System.out.println("Solde du compte Epargne : " + soldeEpargne + "€" + " avec un taux d'intérêt de " + tauxInteret + "%");
+
+
+
+    }
+
+    public void calculInteret(){
+        double interetsAjouter = (soldeEpargne * tauxInteret) / 100;
+        double nouveauSolde = soldeEpargne + interetsAjouter;
+        System.out.print("Intérêts ajoutés: " + interetsAjouter + " Nouveau solde : " + " " + nouveauSolde);
+
+
+    }
+
+
+    public static void main(String[] args) {
+        BanqueExercice be = new BanqueExercice(100, 200, 2);
+        be.calculInteret();
+    }
+
+
+}
