bb663bdd892c3a60a2d9b314f2f29a57547ef5c3
nilswen <nilswentingpro@gmail.com>
2023-09-11 15:23:36 +0200

    .

--- jour4/job03/SynchronizationExercice.java
+++ jour4/job03/SynchronizationExercice.java
@@ -1,12 +1,11 @@
 package jour4.job03;
 
 public class SynchronizationExercice extends Thread{
-    private static int soldeInitial;
+    public static int soldeInitial;
     int retrait;
 
 
-    public SynchronizationExercice(int soldeInitial, int retrait){
-        this.soldeInitial = soldeInitial;
+    public SynchronizationExercice(int retrait){
         this.retrait = retrait;
     }
 
@@ -14,32 +13,46 @@ public class SynchronizationExercice extends Thread{
 
     @Override
     public void run(){
-        retrait(soldeInitial, retrait);
+        retrait();
     }
 
 
 
-    public void retrait(int soldeInitial, int retrait) {
-        int solde = soldeInitial - retrait;
-        System.out.println("Solde final: " + solde);
+    public synchronized void retrait() {
+        soldeInitial -= retrait;
+
+
     }
 
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         if (args.length <= 0){
             System.out.println("Veuillez fournir un argument pour definir le solde du compte en banque ainsi que le montant a retiré");
             return;
         }
 
-        int soldeInitial = Integer.parseInt(args[0]);
+        soldeInitial = Integer.parseInt(args[0]);
         int retrait = Integer.parseInt(args[1]);
 
-        SynchronizationExercice thread1 = new SynchronizationExercice(soldeInitial, retrait);
-        SynchronizationExercice thread2 = new SynchronizationExercice(soldeInitial, retrait);
+        SynchronizationExercice thread1 = new SynchronizationExercice(retrait);
+        SynchronizationExercice thread2 = new SynchronizationExercice(retrait);
         thread1.start();
+        System.out.println(thread1.getName() + "a retiré" + retrait);
+
+
         thread2.start();
+        System.out.println(thread2.getName() + "a retiré" + retrait);
+
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        thread2.join();
+
+        System.out.println("solde final: " + soldeInitial);
 
 
 
